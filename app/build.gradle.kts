88061c5cb0742b93a6d75fe23fa23374193263fd
Denis Šajnar <denis.sajnar@gmail.com>
2025-08-25 22:40:35 +0200

    [feature/ds-fix-native-builds]: Enhance native build support and memory settings
    - Update `gradle.properties` to configure JVM arguments including heap dump on OOM
    - Refactor `bootBuildImage` tasks to simplify configuration in `build.gradle.kts`
    - Add GraalVM native configuration for improved native image support
    - Include `spring-boot-starter-webflux` as dependency in `spring-boot-application`
    
    Signed-off-by: Denis Šajnar <denis.sajnar@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,56 @@
+import org.springframework.boot.buildpack.platform.build.PullPolicy
+
 plugins {
     id("buildlogic.spring-boot-application")
     id("buildlogic.quality-assurance")
     id("buildlogic.code-formatting")
+}
+
+tasks.bootBuildImage {
+    builder.set("paketobuildpacks/builder-jammy-buildpackless-tiny:latest")
+    if (project.hasProperty("native")) {
+        buildpacks.set(
+            listOf(
+                "paketobuildpacks/java-native-image"
+            )
+        )
+    }
+
+    environment.set(
+        mutableMapOf<String, String>(
+            "BP_JVM_VERSION" to "24",
+            "BP_JVM_TYPE" to "JRE"
+        ).apply {
+            if (project.hasProperty("native")) {
+                put("BP_NATIVE_IMAGE", "true")
+                put(
+                    "BP_NATIVE_IMAGE_BUILD_ARGUMENTS",
+                    """
+                    --no-fallback
+                    --enable-url-protocols=http,https
+                    -H:+ReportExceptionStackTraces
+                    """.trimIndent().replace("\n", " ")
+                )
+            }
+        }
+    )
+
+    pullPolicy.set(PullPolicy.IF_NOT_PRESENT)
+
+    imageName.set("${project.group}/${project.name}:${project.version}")
+
+    tags.set(
+        listOf(
+            "${project.group}/${project.name}:latest",
+            "${project.group}/${project.name}:${project.version}"
+        )
+    )
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            imageName.set("${project.group}.${project.name}")
+        }
+    }
 }
